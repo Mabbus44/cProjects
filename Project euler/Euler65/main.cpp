5a76d540b66c5aaed8dfa5cda8b16867dacf3819
Mabbus44 <rasun44@hotmail.com>
2021-11-02 21:44:30 +0100

    Euler65

--- Project euler/Euler65/main.cpp
+++ Project euler/Euler65/main.cpp
@@ -1,47 +1,39 @@
 #include <iostream>
 #include <list>
-#include "PrimeList.h"
-#include "Fraction.h"
-#include <gmpxx.h>
+#include "Number.h"
 
 using namespace std;
 
 int main(){
-  mpz_class a, b, c;
-  a=10;
-  b=20;
-  c=30;
-  cout << a << endl;
-  return 0;
-}
-int mainnotused()
-{
-  __int64* ek = new __int64 [100];
-  PrimeList p(10000000);
-  for(int iterations = 1; iterations<40; iterations++){
-    __int64 k = 2;
-    int i=1;
-    ek[0] = 2;
-    while(i<100){
-      ek[i] = 1;
-      i++;
-      if(i<100)
-        ek[i] = k;
-      k+=2;
-      i++;
-      if(i<100)
-        ek[i] = 1;
-      i++;
+  int k[100];
+  int m2 = 2;
+  int step = 0;
+  k[0] = 2;
+  for(int i=1; i<100; i++){
+    switch(step){
+      case 0:
+      case 2:
+        k[i] = 1;
+        break;
+      case 1:
+        k[i] = m2;
+        m2 += 2;
+        break;
     }
-    Fraction f1(&p, ek[iterations-1], 1);
-    Fraction f2(&p, 1, 1);
-    for(i=iterations-2; i>=0; i--){
-      f1.invert();
-      f2.setInteger(ek[i]);
-      f1 = f1+f2;
-    }
-    cout << "Iterations: " << iterations << endl;
-    f1.print();
+    step++;
+    if(step>2)
+      step = 0;
   }
-  return 0;
+
+  Number tp0;
+  Number tp1;
+  tp1 = k[99];
+  Number tp2;
+  tp2 = 1;
+  for(int i=98; i>=0; i--){
+    tp0 = tp1*k[i]+tp2;
+    tp2 = tp1;
+    tp1 = tp0;
+  }
+  cout << "sum: " << tp0.digitalSum() << endl;
 }
