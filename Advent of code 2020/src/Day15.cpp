2e8229bcf9f61474ba6f290dba721559ac90d98e
Mabbus44 <rasun44@hotmail.com>
2022-05-14 20:41:33 +0200

    AoC2020 Day15

--- Advent of code 2020/src/Day15.cpp
+++ Advent of code 2020/src/Day15.cpp
@@ -1,4 +1,5 @@
 #include "Day15.h"
+#include <map>
 
 Day15::Day15()
 {
@@ -11,11 +12,37 @@ Day15::~Day15()
 }
 
 void Day15::part1(string filename){
-  InputLoader loader(filename);
-  cout << "part 1: " << endl;
+  vector<int> series = {16,11,15,0,1,7};
+  int index = 6;
+
+  while(index<2020){
+    int last = series[index-1];
+    int index2 = index-2;
+    while(index2 >= 0 && series[index2] != last)
+      index2--;
+    if(index2<0)
+      series.push_back(0);
+    else
+      series.push_back(index-1-index2);
+    index++;
+  }
+  cout << "part 1: " << series[2019] << endl;
 }
 
 void Day15::part2(string filename){
-  InputLoader loader(filename);
-  cout << "part 2: " << endl;
+  map<__int64, __int64> series = {{16, 0},{11, 1},{15, 2},{0, 3},{1, 4}};
+  __int64 last = 7;
+  __int64 index = 6;
+  __int64 next;
+
+  while(index<30000000){
+    if(!series.count(last))
+      next = 0;
+    else
+      next = index-1-series[last];
+    series[last] = index-1;
+    last = next;
+    index++;
+  }
+  cout << "part 2: " << next << endl;
 }
