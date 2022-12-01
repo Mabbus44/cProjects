4f3483c02c42fd671147fe441ffaa74c0cce2ccb
Mabbus44 <rasun44@hotmail.com>
2022-12-01 07:51:56 +0100

    AOC2022 Day01

--- Advent of code 2022/src/Day01.cpp
+++ Advent of code 2022/src/Day01.cpp
@@ -11,9 +11,62 @@ Day01::~Day01()
 }
 
 void Day01::part1(string filename){
-  cout << "part 1: " << endl;
+  InputLoader loader(filename);
+  vector<string> depthsStr = loader.toVectorStr();
+  vector<vector<int>> elfs;
+  vector<int> currentElf;
+  for(string s: depthsStr){
+    if(s.length() > 0)
+      currentElf.push_back(stoi(loader.trim(s)));
+    else{
+      elfs.push_back(currentElf);
+      currentElf.clear();
+    }
+  }
+  int biggestSum = 0;
+  for(vector<int> elf: elfs){
+    int sum = 0;
+    for(int i: elf){
+      sum += i;
+    }
+    if(sum>biggestSum)
+      biggestSum = sum;
+  }
+
+  cout << "part 1: " << biggestSum << endl;
 }
 
 void Day01::part2(string filename){
-  cout << "part 2: " << endl;
+  InputLoader loader(filename);
+  vector<string> depthsStr = loader.toVectorStr();
+  vector<vector<int>> elfs;
+  vector<int> currentElf;
+  for(string s: depthsStr){
+    if(s.length() > 0)
+      currentElf.push_back(stoi(loader.trim(s)));
+    else{
+      elfs.push_back(currentElf);
+      currentElf.clear();
+    }
+  }
+  int biggestSum[3] = {0,0,0};
+  for(vector<int> elf: elfs){
+    int sum = 0;
+    for(int i: elf){
+      sum += i;
+    }
+    for(int i=0; i<3; i++){
+      if(sum > biggestSum[i]){
+        for(int i2=2; i2>i; i2--){
+          biggestSum[i2] = biggestSum[i2-1];
+        }
+        biggestSum[i] = sum;
+        break;
+      }
+    }
+  }
+  int totalSum = 0;
+  for(int i=0; i<3; i++)
+    totalSum += biggestSum[i];
+  cout << "part 2: " << totalSum << endl;
 }
