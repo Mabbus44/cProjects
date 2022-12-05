73bfad3613a9ed14007d84545cd31ac49824a227
Mabbus44 <rasun44@hotmail.com>
2022-12-05 21:10:03 +0100

    AOC2022 Day04

--- Advent of code 2022/main.cpp
+++ Advent of code 2022/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "Common.h"
-#include "Day03.h"
+#include "Day04.h"
 
 using namespace std;
 
 int main()
 {
-    Day03 d;
-    d.part1("input03.txt");
-    d.part2("input03.txt");
+    Day04 d;
+    d.part1("input04.txt");
+    d.part2("input04.txt");
     return 0;
 }
--- Advent of code 2022/src/Day04.cpp
+++ Advent of code 2022/src/Day04.cpp
@@ -11,9 +11,26 @@ Day04::~Day04()
 }
 
 void Day04::part1(string filename){
-  cout << "part 1: " << endl;
+  InputLoader loader(filename);
+  vector<vector<int>> elfPairs = loader.toVectorInt2d({"-", ","});
+  int count = 0;
+  for(vector<int> pair: elfPairs){
+    if((pair[0] <= pair[2] && pair[1] >= pair[3]) ||
+       (pair[2] <= pair[0] && pair[3] >= pair[1]))
+      count++;
+  }
+  cout << "part 1: " << count << endl;
 }
 
 void Day04::part2(string filename){
-  cout << "part 2: " << endl;
+  InputLoader loader(filename);
+  vector<vector<int>> elfPairs = loader.toVectorInt2d({"-", ","});
+  int count = 0;
+  for(vector<int> pair: elfPairs){
+    if((pair[2] >= pair[0] && pair[2] <= pair[1]) ||
+       (pair[3] >= pair[0] && pair[3] <= pair[1]) ||
+       (pair[2] < pair[0] && pair[3] > pair[1]))
+      count++;
+  }
+  cout << "part 2: " << count << endl;
 }
