687f48f609b6aad712169f09401dd7a1f731580f
Mabbus44 <rasun44@hotmail.com>
2022-12-05 22:12:13 +0100

    AOC2022 Day05

--- Advent of code 2022/include/Day05.h
+++ Advent of code 2022/include/Day05.h
@@ -2,6 +2,7 @@
 #define DAY05_H
 #include <iostream>
 #include <vector>
+#include <list>
 #include "Common.h"
 
 using namespace std;
@@ -13,6 +14,11 @@ class Day05
     virtual ~Day05();
     void part1(string filename);
     void part2(string filename);
+    void moveCrates(int count, int from, int to);
+    void moveCrates2(int count, int from, int to);
+    void clearStacks();
+    list<char> string2list(string s);
+    vector<list<char>> stacks;
 
   protected:
 
--- Advent of code 2022/src/Day05.cpp
+++ Advent of code 2022/src/Day05.cpp
@@ -2,7 +2,7 @@
 
 Day05::Day05()
 {
-  //ctor
+  clearStacks();
 }
 
 Day05::~Day05()
@@ -11,9 +11,72 @@ Day05::~Day05()
 }
 
 void Day05::part1(string filename){
-  cout << "part 1: " << endl;
+  InputLoader loader(filename);
+  vector<vector<int>> moves = loader.toVectorInt2d({"move ", " from ", " to "});
+  for(vector<int> move:moves){
+    moveCrates(move[0], move[1]-1, move[2]-1);
+  }
+  string ans="";
+  for(list<char> stack: stacks)
+    if(stack.size() > 0)
+      ans = ans + stack.back();
+  cout << "part 1: " << ans << endl;
 }
 
 void Day05::part2(string filename){
-  cout << "part 2: " << endl;
+  clearStacks();
+  InputLoader loader(filename);
+  vector<vector<int>> moves = loader.toVectorInt2d({"move ", " from ", " to "});
+  for(vector<int> move:moves){
+    moveCrates2(move[0], move[1]-1, move[2]-1);
+  }
+  string ans="";
+  for(list<char> stack: stacks)
+    if(stack.size() > 0)
+      ans = ans + stack.back();
+  cout << "part 2: " << ans << endl;
+}
+
+void Day05::clearStacks(){
+  stacks.clear();
+  stacks.push_back(string2list("SLW"));
+  stacks.push_back(string2list("JTNQ"));
+  stacks.push_back(string2list("SCHFJ"));
+  stacks.push_back(string2list("TRMWNGB"));
+  stacks.push_back(string2list("TRLSDHQB"));
+  stacks.push_back(string2list("MJBVFHRL"));
+  stacks.push_back(string2list("DWRNJM"));
+  stacks.push_back(string2list("BZTFHNDJ"));
+  stacks.push_back(string2list("HLQNBFT"));
+}
+
+list<char> Day05::string2list(string s){
+  list<char> ret;
+  for(char c:s){
+    ret.push_back(c);
+  }
+  return ret;
+}
+
+void Day05::moveCrates(int count, int from, int to){
+  for(int i=0; i<count; i++){
+    stacks[to].push_back(stacks[from].back());
+    stacks[from].pop_back();
+    if(stacks[from].size() == 0)
+      return;
+  }
+}
+
+void Day05::moveCrates2(int count, int from, int to){
+  if(count>stacks[from].size())
+    count = stacks[from].size();
+  list<char>::iterator moveIt = stacks[from].end();
+  for(int i=0; i<count; i++)
+    moveIt--;
+  for(int i=0; i<count; i++){
+    stacks[to].push_back(*moveIt);
+    moveIt++;
+  }
+  for(int i=0; i<count; i++)
+    stacks[from].pop_back();
 }
