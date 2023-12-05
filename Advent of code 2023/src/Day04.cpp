d6088d003d32bea625fcf25e3910a3049c7eb50d
Mabbus44 <rasun44@hotmail.com>
2023-12-05 09:31:10 +0100

    AoC2023Day04

--- Advent of code 2023/src/Day04.cpp
+++ Advent of code 2023/src/Day04.cpp
@@ -12,10 +12,71 @@ Day04::~Day04()
 
 void Day04::part1(string filename){
   InputLoader loader(filename);
-  cout << "part 1: " << endl;
+  vector<vector<string>> input = loader.toVectorStr2d({":", "|"});
+  vector<vector<int>> yourNumbers;
+  vector<vector<int>> winningNumbers;
+  for(vector<string> row: input){
+    vector<string> yourNumbersStrings = InputLoader::splitString(row[1], {" "});
+    vector<string> winningNumbersStrings = InputLoader::splitString(row[2], {" "});
+    vector<int> yourNumbersOne;
+    vector<int> winningNumbersOne;
+    for(string num: yourNumbersStrings)
+      yourNumbersOne.push_back(stoi(num));
+    for(string num: winningNumbersStrings)
+      winningNumbersOne.push_back(stoi(num));
+    yourNumbers.push_back(yourNumbersOne);
+    winningNumbers.push_back(winningNumbersOne);
+  }
+  int totalScore = 0;
+  for(int i=0; i<(int)yourNumbers.size(); i++){
+    int score = 0;
+    for(int num: yourNumbers[i]){
+      if(find(winningNumbers[i].begin(), winningNumbers[i].end(), num) != winningNumbers[i].end()){
+        if(score == 0)
+          score = 1;
+        else
+          score *= 2;
+      }
+    }
+    totalScore += score;
+  }
+  cout << "part 1: " << totalScore << endl;
 }
 
 void Day04::part2(string filename){
   InputLoader loader(filename);
-  cout << "part 2: " << endl;
+  vector<vector<string>> input = loader.toVectorStr2d({":", "|"});
+  vector<vector<int>> yourNumbers;
+  vector<vector<int>> winningNumbers;
+  for(vector<string> row: input){
+    vector<string> yourNumbersStrings = InputLoader::splitString(row[1], {" "});
+    vector<string> winningNumbersStrings = InputLoader::splitString(row[2], {" "});
+    vector<int> yourNumbersOne;
+    vector<int> winningNumbersOne;
+    for(string num: yourNumbersStrings)
+      yourNumbersOne.push_back(stoi(num));
+    for(string num: winningNumbersStrings)
+      winningNumbersOne.push_back(stoi(num));
+    yourNumbers.push_back(yourNumbersOne);
+    winningNumbers.push_back(winningNumbersOne);
+  }
+  vector<int> cardCount(yourNumbers.size(), 1);
+  for(int i=0; i<(int)yourNumbers.size(); i++){
+    int count = 0;
+    for(int num: yourNumbers[i]){
+      if(find(winningNumbers[i].begin(), winningNumbers[i].end(), num) != winningNumbers[i].end()){
+        count++;
+      }
+    }
+    for(int cardId=i+1; cardId < i + count + 1; cardId++){
+      if(cardId < (int)cardCount.size()){
+        cardCount[cardId] += cardCount[i];
+      }
+    }
+  }
+  int totalCount = 0;
+  for(int count: cardCount){
+    totalCount += count;
+  }
+  cout << "part 2: " << totalCount << endl;
 }
