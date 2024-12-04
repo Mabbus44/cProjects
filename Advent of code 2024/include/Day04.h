4faa8507a522020b1150f2322e6e6b88828bffae
Rasmus Ingemarsson <rasun44@hotmail.com>
2024-12-04 07:33:26 +0100

    AoC2024Day04

--- Advent of code 2024/include/Day04.h
+++ Advent of code 2024/include/Day04.h
@@ -9,7 +9,9 @@ class Day04
     virtual ~Day04();
     void part1(std::string filename);
     void part2(std::string filename);
-
+    bool findXmas(int x, int y, int dX, int dY);
+    bool findXdashMas(int x, int y);
+    std::vector<std::vector<char>> input;
   protected:
 
   private:
--- Advent of code 2024/src/Day04.cpp
+++ Advent of code 2024/src/Day04.cpp
@@ -11,9 +11,69 @@ Day04::~Day04()
 }
 
 void Day04::part1(std::string filename){
-  std::cout << "part 1: " << std::endl;
+  InputLoader loader(filename);
+  input = loader.toVectorChar2d();
+  int xMasCount = 0;
+  for(int x=0; x<(int)input[0].size(); x++){
+    for(int y=0; y<(int)input.size(); y++){
+      for(int dX=-1; dX <= 1; dX++){
+        for(int dY=-1; dY <= 1; dY++){
+          if(findXmas(x, y, dX, dY))
+            xMasCount++;
+        }
+      }
+    }
+  }
+  std::cout << "part 1: " << xMasCount << std::endl;
 }
 
 void Day04::part2(std::string filename){
-  std::cout << "part 2: " << std::endl;
+  InputLoader loader(filename);
+  input = loader.toVectorChar2d();
+  int xMasCount = 0;
+  for(int x=0; x<(int)input[0].size(); x++){
+    for(int y=0; y<(int)input.size(); y++){
+      if(findXdashMas(x, y))
+        xMasCount++;
+    }
+  }
+  std::cout << "part 2: " << xMasCount << std::endl;
+}
+
+bool Day04::findXmas(int x, int y, int dX, int dY){
+  int wl=3;
+  if(x+wl*dX<0 || x+wl*dX>=(int)input[0].size() || y+wl*dY<0 || y+wl*dY>=(int)input.size())
+    return false;
+  if(dX==0 && dY==0)
+    return false;
+  std::string word="XMAS";
+  int pos=0;
+  while(pos<(int)word.length()){
+    if(input[y][x] != word[pos]){
+      return false;
+    }
+    pos++;
+    x+=dX;
+    y+=dY;
+  }
+  return true;
+}
+
+bool Day04::findXdashMas(int x, int y){
+  int wl=1;
+  if(x-1<0 || x+1>=(int)input[0].size() || y-1<0 || y+1>=(int)input.size())
+    return false;
+  if(input[y][x] != 'A')
+    return false;
+  char tl=input[y-1][x-1];
+  char tr=input[y-1][x+1];
+  char bl=input[y+1][x-1];
+  char br=input[y+1][x+1];
+
+  if((tl == 'M' && br == 'S') || (tl == 'S' && br == 'M')){
+    if((tr == 'M' && bl == 'S') || (tr == 'S' && bl == 'M')){
+        return true;
+    }
+  }
+  return false;
 }
